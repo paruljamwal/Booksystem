003b9a1a5eb9cd3913f0e4df157bfc46af268a84
paruljamwal <paruljamwal2000@gmail.com>
2022-03-21 10:56:34 +0530

    final submission

--- src/controller/authcontroller.js
+++ src/controller/authcontroller.js
@@ -1,10 +1,20 @@
 const {register,login}=require("../model/authmodel");
+const bcrypt = require('bcrypt');
 const express=require("express");
+const usermodel = require("../model/Usermodel");
 const router=express.Router();
 
 router.post("",async(req,res)=>{
     try{
         const register= await usermodel.create(req.body);
+        const email=await usermodel.find(req.body.email);
+        if(!email){
+            return res.status(404).send("Wrong email or password");
+        }
+
+        else{
+            
+        }
         return res.status(202).send({register:register});
     }
     catch(err){
